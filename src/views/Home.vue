98782c52060e3a871c9bb2fb1e6693f26b47094c
Lucas Hünniger <lucas.huenniger02@gmail.com>
2025-01-16 23:53:40 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-center items-center h-screen bg-gray-800 text-white">
+    <div class="text-center">
+      <h1 class="text-5xl font-bold">Lucas Hünniger</h1>
+      <p class="mt-2 text-xl">Software Engineer</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+</style>
