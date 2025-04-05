80c71f123311ff88ecaa723ddcac7b058930d649
Lucas Hünniger <lucas.huenniger02@gmail.com>
2025-04-05 23:36:50 +0200

    feat: add basic experience entries add visual representation

---
+++ src/components/ExperienceEntry.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <p>{{ company }}</p>
+    <p>{{ startDate }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: Array,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/components/sections/ExperiencesSection.vue
+++ src/components/sections/ExperiencesSection.vue
@@ -1,5 +1,32 @@
 <template>
-  <section class="min-h-screen flex flex-col justify-center">
-    <p>ExperiencesSection</p>
+  <section class="min-h-screen flex flex-col justify-center space-y-6">
+    <ExperienceEntry
+      v-for="experience in experiences"
+      :key="experience.id"
+      :title="experience.title"
+      :company="experience.company"
+      :startDate="experience.startDate"
+      :endDate="experience.endDate"
+      :description="experience.description"
+      :skills="experience.skills"
+      :link="experience.link"
+    />
   </section>
 </template>
+
+<script>
+import ExperienceEntry from "@/components/ExperienceEntry.vue";
+import { experiences } from "@/data/content.js";
+
+export default {
+  name: "ExperiencesSection",
+  components: {
+    ExperienceEntry,
+  },
+  data() {
+    return {
+      experiences: experiences,
+    };
+  },
+};
+</script>
