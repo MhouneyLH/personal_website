41de4b07a13a36c641549787a9e2b0436e3edea0
Lucas Hünniger <lucas.huenniger02@gmail.com>
2025-10-10 12:54:41 +0200

    fix:  remove weird basePath configuration

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-// see router.js for handling different base url
-// when e. g. deployed to github pages...
-let basePath = "/";
-if (process.env.NODE_ENV === "production") {
-  basePath = "/portfolio/";
-}
-
-// docker has problems with resolving /personal_website/ ...
-if (process.env.DOCKER_ENV === "true") {
-  basePath = "/";
-}
-
-console.log("Using base path: ", basePath);
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: basePath,
+  base: "/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
